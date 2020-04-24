905b7601a1f02d9b68f49c042c1b0f1c4a862e20
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-24 20:29:54 +0200

    detail view on architects/:id

--- routes/architects.routes.js
+++ routes/architects.routes.js
@@ -2,10 +2,22 @@ const express = require('express')
 const router = express.Router()
 const Architect = require('./../models/architect.model')
 
+//Listing and detail view
+
 router.get('/', (req, res, next) => {
   Architect.find()
     .then((architects) => {
       res.render('./architects/index', { architects })
+    })
+    .catch((err) => {
+      next(new Error(err))
+    })
+})
+
+router.get('/:id', (req, res, next) => {
+  Architect.findById(req.params.id)
+    .then((architect) => {
+      res.render('./architects/detail', architect)
     })
     .catch((err) => {
       next(new Error(err))
