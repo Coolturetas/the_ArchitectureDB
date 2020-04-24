7a2138aad5604d2991eaf7cb7857123fcc2f0c23
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-24 20:17:49 +0200

    architects/ endpoint has a view, still needs some buttons and love

--- routes/architects.routes.js
+++ routes/architects.routes.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = express.Router()
 const Architect = require('./../models/architect.model')
 
-router.get("/", (req, res, next) => res.render("./architects/index"))
+router.get('/', (req, res, next) => {
+  Architect.find()
+    .then((architects) => {
+      res.render('./architects/index', { architects })
+    })
+    .catch((err) => {
+      next(new Error(err))
+    })
+})
 
 module.exports = router
