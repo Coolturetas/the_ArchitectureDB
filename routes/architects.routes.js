d98e4d186de27633cb3bfe876e88e3d111d619b2
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-25 10:21:06 +0200

    redefine routes for better understanding, create delete route

--- routes/architects.routes.js
+++ routes/architects.routes.js
@@ -20,6 +20,45 @@ router.post('/create', (req, res, next) => {
     })
 })
 
+//Edition
+
+router.get('/edit/:id', (req, res, next) => {
+  Architect.findById(req.params.id)
+    .then((architect) => {
+      res.render('./architects/edit', architect)
+    })
+    .catch((err) => {
+      next(new Error(err))
+    })
+})
+
+router.post('/edit/:id', (req, res, next) => {
+  const { name, country, flagshipWork } = req.body
+
+  Architect.findByIdAndUpdate(
+    req.params.id,
+    { name, country, flagshipWork },
+    { new: true }
+  )
+    .then((architect) => {
+      res.redirect(`/architects/view/${architect._id}`)
+    })
+    .catch((err) => {
+      next(new Error(err))
+    })
+})
+
+//Deletion
+
+router.post("/delete/:id", (req, res, next) => {
+  Architect.findByIdAndDelete(req.params.id)
+  .then(() => {
+    res.redirect("/architects")
+  }).catch((err) => {
+    next(new Error(err))
+  });
+})
+
 //Listing and detail view
 
 router.get('/', (req, res, next) => {
@@ -32,7 +71,7 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/view/:id', (req, res, next) => {
   Architect.findById(req.params.id)
     .then((architect) => {
       res.render('./architects/detail', architect)
@@ -42,32 +81,5 @@ router.get('/:id', (req, res, next) => {
     })
 })
 
-//Edition
-
-router.get('/:id/edit', (req, res, next) => {
-  Architect.findById(req.params.id)
-    .then((architect) => {
-      res.render('./architects/edit', architect)
-    })
-    .catch((err) => {
-      next(new Error(err))
-    })
-})
-
-router.post('/:id/edit', (req, res, next) => {
-  const { name, country, flagshipWork } = req.body
-
-  Architect.findByIdAndUpdate(
-    req.params.id,
-    { name, country, flagshipWork },
-    { new: true }
-  )
-    .then((architect) => {
-      res.redirect(`/architects/${architect._id}`)
-    })
-    .catch((err) => {
-      next(new Error(err))
-    })
-})
 
 module.exports = router
