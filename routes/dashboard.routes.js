53c25a8994f19e1bf4628261666c7076f3fcf9d3
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-25 18:36:36 +0200

    validation dashboard only accessible to editors and admins

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -5,16 +5,21 @@ const Trend = require('./../models/trend.model')
 const User = require('./../models/user.model')
 const passport = require('passport')
 
-function checkAuth(req, res, next) {
-  return req.isAuthenticated() ? next() : res.redirect('/login')
+const checkIsInRole = (...roles) => (req, res, next) => {
+  if (!req.user) {
+    return res.redirect('/login')
+  }
+
+  const hasRole = roles.find((role) => req.user.role === role)
+  if (!hasRole) {
+    return res.redirect('/login')
+  }
+
+  return next()
 }
 
-function checkRoles(role) {
-  return function (req, res, next) {
-    return req.isAuthenticated() && req.user.role === role
-      ? next()
-      : res.redirect('/login')
-  }
+function checkAuth(req, res, next) {
+  return req.isAuthenticated() ? next() : res.redirect('/login')
 }
 
 //Access to dashboard
@@ -22,5 +27,11 @@ function checkRoles(role) {
 router.get('/', checkAuth, (req, res, next) =>
   res.render('./dashboard/index', { user: req.user })
 )
+
+//access to validation screen
+
+router.get('/validate', checkIsInRole("editor", "admin"), (req, res, next) => {
+  res.render("./dashboard/validation-screen")
+})
 
 module.exports = router
