6d4b614d489c0c1f4b8f7a47e07b0adbf5f65f24
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-25 14:54:57 +0200

    add routing to dashboard on /dashboard

---
+++ routes/dashboard.routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const Architect = require('./../models/architect.model')
+const Trend = require('./../models/trend.model')
+const User = require('./../models/user.model')
+const passport = require('passport')
+
+function checkAuth(req, res, next) {
+  return req.isAuthenticated() ? next() : res.redirect('/login')
+}
+
+function checkRoles(role) {
+  return function (req, res, next) {
+    return req.isAuthenticated() && req.user.role === role
+      ? next()
+      : res.redirect('/login')
+  }
+}
+
+//Access to dashboard
+
+router.get('/', checkAuth, (req, res, next) => res.render('./dashboard/index'))
+
+module.exports = router
