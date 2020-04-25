bfdfa2182f19ef91ac5bc04072bb4cc4afd99e59
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-25 13:27:50 +0200

    authentication is checked out before accessing edition/creation/deletion

--- routes/architects.routes.js
+++ routes/architects.routes.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const Architect = require('./../models/architect.model')
+const passport = require('passport')
+
+function checkAuth(req, res, next) {
+  return req.isAuthenticated() ? next() : res.redirect('/login')
+}
+
+function checkRoles(role) {
+  return function (req, res, next) {
+    return req.isAuthenticated() && req.user.role === role
+      ? next()
+      : res.redirect('/login')
+  }
+}
 
 //Creation
 
-router.get('/create', (req, res, next) => {
+router.get('/create', checkAuth, (req, res, next) => {
   res.render('./architects/create')
 })
 
-router.post('/create', (req, res, next) => {
+router.post('/create', checkAuth, (req, res, next) => {
   const { name, country, flagshipWork } = req.body
 
   Architect.create({ name, country, flagshipWork })
@@ -22,7 +35,7 @@ router.post('/create', (req, res, next) => {
 
 //Edition
 
-router.get('/edit/:id', (req, res, next) => {
+router.get('/edit/:id', checkAuth, (req, res, next) => {
   Architect.findById(req.params.id)
     .then((architect) => {
       res.render('./architects/edit', architect)
@@ -32,7 +45,7 @@ router.get('/edit/:id', (req, res, next) => {
     })
 })
 
-router.post('/edit/:id', (req, res, next) => {
+router.post('/edit/:id', checkAuth, (req, res, next) => {
   const { name, country, flagshipWork } = req.body
 
   Architect.findByIdAndUpdate(
@@ -50,13 +63,14 @@ router.post('/edit/:id', (req, res, next) => {
 
 //Deletion
 
-router.post("/delete/:id", (req, res, next) => {
+router.post('/delete/:id', checkAuth, (req, res, next) => {
   Architect.findByIdAndDelete(req.params.id)
-  .then(() => {
-    res.redirect("/architects")
-  }).catch((err) => {
-    next(new Error(err))
-  });
+    .then(() => {
+      res.redirect('/architects')
+    })
+    .catch((err) => {
+      next(new Error(err))
+    })
 })
 
 //Listing and detail view
@@ -80,6 +94,5 @@ router.get('/view/:id', (req, res, next) => {
       next(new Error(err))
     })
 })
-
 
 module.exports = router
