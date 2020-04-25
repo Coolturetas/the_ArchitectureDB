a4cb9f136c36c2469742c85789bc41cc011ee0d7
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-25 17:49:17 +0200

    modify models to add verification status

--- models/architect.model.js
+++ models/architect.model.js
@@ -1,14 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const architectSchema = new Schema({
-  name: {type: String, required: true, unique:true},
+  name: { type: String, required: true, unique: true },
   country: String,
   flagshipWork: String,
   description: String,
   works: Array,
   photo: String,
+  isVerified: { type: Boolean, default: false },
 })
 
-const Architect = mongoose.model("Architect", architectSchema)
+const Architect = mongoose.model('Architect', architectSchema)
 module.exports = Architect
--- models/trend.model.js
+++ models/trend.model.js
@@ -2,17 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const trendSchema = new Schema(
-	{
-		name: String,
-		picTrend: String,
-		country: String,
-		description: String,
-		bestWork: String,
-		year: Number,
-	},
-	{
-		timestamps: true,
-	}
+  {
+    name: String,
+    picTrend: String,
+    country: String,
+    description: String,
+    bestWork: String,
+    year: Number,
+    isVerified: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
 )
 
 const Trend = mongoose.model('trend', trendSchema)
