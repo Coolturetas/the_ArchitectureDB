80130146a4607b39243ab3d72b1de214d23b8fd7
Yai <myd.rio@hotmail.com>
2020-04-25 12:03:23 +0200

    First installing cloudinary

--- routes/trend.routes.js
+++ routes/trend.routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const uploadLocal = multer({ dest: './public/uploads/' })
+// const multer = require('multer')
+// const uploadLocal = multer({ dest: './public/uploads/' })
 const Trend = require('../models/trend.model')
 const cloudUploader = require('../configs/cloudinary.config')
 
@@ -11,7 +11,7 @@ router.get('/', (req, res, next) => {
 })
 
 //Add new architecht
-router.get('/new', (req, res, next) => res.render('archTrend/at-add'))
+router.get('/new', (req, res, next) => res.render('archTrend/at-add', { dest: 'CDN', path: 'upload' }))
 router.post('/', uploadLocal.single('picTrend'), (req, res, next) => {
 	const newTrend = {
 		name: req.body.name,
@@ -25,6 +25,20 @@ router.post('/', uploadLocal.single('picTrend'), (req, res, next) => {
 		.then((data) => res.redirect('/trend'))
 		.catch((err) => next(new Error('No se ha creado nada, torpe', err)))
 })
+// router.get('/new', (req, res, next) => res.render('archTrend/at-add'))
+// router.post('/', uploadLocal.single('picTrend'), (req, res, next) => {
+// 	const newTrend = {
+// 		name: req.body.name,
+// 		picTrend: `uploads/${req.file.filename}`,
+// 		description: req.body.description,
+// 		country: req.body.country,
+// 		bestWork: req.body.bestWork,
+// 		year: req.body.year,
+// 	}
+// 	Trend.create(newTrend)
+// 		.then((data) => res.redirect('/trend'))
+// 		.catch((err) => next(new Error('No se ha creado nada, torpe', err)))
+// })
 
 //Show details of each architect
 router.get('/show/:id', (req, res, next) => {
@@ -32,5 +46,7 @@ router.get('/show/:id', (req, res, next) => {
 		.then((archId) => res.render('archTrend/at-dets', archId))
 		.catch((err) => next(new Error('No se puede mostrar nada', err)))
 })
+
+//Delete
 
 module.exports = router
