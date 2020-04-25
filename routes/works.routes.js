135ff18686f33327bff878d753f780e354144ce9
Yai <myd.rio@hotmail.com>
2020-04-25 19:46:33 +0200

    Works CRUD finished

--- routes/works.routes.js
+++ routes/works.routes.js
@@ -11,6 +11,35 @@ router.get('/', (req, res, next) => {
 		.catch((err) => next(new Error('No se ha encontrado nada', err)))
 })
 
+//Edit
+router.get('/edit/:id', (req, res, next) => {
+	const workPromise = Work.findById(req.params.id).populate('trend').populate('architect')
+	const archPromise = Arch.find()
+	const trendPromise = Trend.find()
+
+	Promise.all([workPromise, archPromise, trendPromise])
+		.then((results) => res.render('works/works-edit', { works: results[0], archs: results[1], trends: results[2] }))
+		.catch((err) => next(new Error('No se ha encontrado nada para editar', err)))
+})
+
+router.post('/edit/:id', cloudUploader.single('photo-work'), (req, res, next) => {
+	const editWork = {
+		trend: req.body.trend,
+		architect: req.body.architect,
+		name: req.body.name,
+		finished: req.body.finished,
+		where: req.body.where,
+		description: req.body.description,
+		workType: req.body.workType,
+		picWork: req.file.url,
+	}
+
+	Work.findByIdAndUpdate(req.params.id, editWork, { new: true })
+		.then(res.redirect('/works'))
+		.catch((err) => next(new Error('No se ha editado nada', err)))
+})
+
+//Add new
 router.get('/new', (req, res, next) => {
 	const allPromise = [Arch.find(), Trend.find()]
 
@@ -25,6 +54,7 @@ router.post('/', cloudUploader.single('photo-work'), (req, res, next) => {
 		architect: req.body.architect,
 		name: req.body.name,
 		finished: req.body.finished,
+		where: req.body.where,
 		description: req.body.description,
 		workType: req.body.workType,
 		picWork: req.file.url,
@@ -33,6 +63,22 @@ router.post('/', cloudUploader.single('photo-work'), (req, res, next) => {
 	Work.create(newWork)
 		.then(res.redirect('/works'))
 		.catch((err) => next(new Error('No se ha creado nada', err)))
+})
+
+//Find One by ID
+router.get('/show/:id', (req, res, next) => {
+	Work.findById(req.params.id)
+		.populate('trend')
+		.populate('architect')
+		.then((workFound) => res.render('works/works-dets', workFound))
+		.catch((err) => next(new Error('No se ha encontrado nada para ver', err)))
+})
+
+//Delete
+router.post('/delete/:id', (req, res, next) => {
+	Work.findByIdAndRemove(req.params.id)
+		.then(res.redirect('/works'))
+		.catch((err) => next(new Error('No se ha borrado nada', err)))
 })
 
 module.exports = router
