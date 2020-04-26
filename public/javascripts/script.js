fdd06884e414c486659947a6fac04f1dfdb36f48
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-26 18:36:05 +0200

    works now are geolocalized on their details page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,21 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    console.log('IronGenerator JS imported successfully!')
+  },
+  false
+)
 
-  console.log('IronGenerator JS imported successfully!');
+const workMap = document.getElementById('workMap')
+const workId = document.getElementById('workId').innerText
 
-}, false);
+let renderedMap
+let markers = []
+let marker
 
+function pinMarker(place, targetMap) {
+  const geocoder = new google.maps.Geocoder()
+  geocoder.geocode({address: place.address}, (results, status) => {
+    if (status === "OK") {
+      targetMap.setCenter(results[0].geometry.location)
+      marker = new google.maps.Marker({
+        map: targetMap,
+        position: results[0].geometry.location,
+        title: place.name
+      })
+      markers.push(marker)
+    } else {
+      console.log("Geocode wasn't successful: ", status)
+    }
+  })
+}
+
+function getPlaces() {
+  axios
+    .get(`/works/api/${workId}`)
+    .then((result) => {
+      pinMarker(result.data.work, renderedMap)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 
 function startMap() {
   const ironhackBCN = {
-  	lat: 41.3977381,
-  	lng: 2.190471916};
-  const map = new google.maps.Map(
-    document.getElementById('workMap'),
-    {
-      zoom: 15,
-      center: ironhackBCN
-    }
-  );
+    lat: 41.3977381,
+    lng: 2.190471916,
+  }
+  renderedMap = new google.maps.Map(workMap, {
+    zoom: 15,
+    center: ironhackBCN,
+  })
+  getPlaces()
 }
- 
-startMap();
+
+startMap()
