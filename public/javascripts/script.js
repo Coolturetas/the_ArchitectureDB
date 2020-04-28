29983c3e0c6a5ccfe2c88aa2c3550a9dac495d98
Yai <myd.rio@hotmail.com>
2020-04-28 15:15:06 +0200

    pre deploy-files

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,9 @@
 document.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    console.log('IronGenerator JS imported successfully!')
-  },
-  false
+	'DOMContentLoaded',
+	() => {
+		console.log('Bienvenidos a the_ArchitectureDB')
+	},
+	false
 )
 
 const workMap = document.getElementById('workMap')
@@ -14,43 +14,43 @@ let markers = []
 let marker
 
 function pinMarker(place, targetMap) {
-  const geocoder = new google.maps.Geocoder()
-  geocoder.geocode({address: place.address}, (results, status) => {
-    if (status === "OK") {
-      targetMap.setCenter(results[0].geometry.location)
-      marker = new google.maps.Marker({
-        map: targetMap,
-        position: results[0].geometry.location,
-        title: place.name
-      })
-      markers.push(marker)
-    } else {
-      console.log("Geocode wasn't successful: ", status)
-    }
-  })
+	const geocoder = new google.maps.Geocoder()
+	geocoder.geocode({ address: place.address }, (results, status) => {
+		if (status === 'OK') {
+			targetMap.setCenter(results[0].geometry.location)
+			marker = new google.maps.Marker({
+				map: targetMap,
+				position: results[0].geometry.location,
+				title: place.name,
+			})
+			markers.push(marker)
+		} else {
+			console.log("Geocode wasn't successful: ", status)
+		}
+	})
 }
 
 function getPlaces() {
-  axios
-    .get(`/works/api/${workId}`)
-    .then((result) => {
-      pinMarker(result.data.work, renderedMap)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+	axios
+		.get(`/works/api/${workId}`)
+		.then((result) => {
+			pinMarker(result.data.work, renderedMap)
+		})
+		.catch((err) => {
+			console.log(err)
+		})
 }
 
 function startMap() {
-  const ironhackBCN = {
-    lat: 41.3977381,
-    lng: 2.190471916,
-  }
-  renderedMap = new google.maps.Map(workMap, {
-    zoom: 15,
-    center: ironhackBCN,
-  })
-  getPlaces()
+	const ironhackBCN = {
+		lat: 41.3977381,
+		lng: 2.190471916,
+	}
+	renderedMap = new google.maps.Map(workMap, {
+		zoom: 15,
+		center: ironhackBCN,
+	})
+	getPlaces()
 }
 
 startMap()
