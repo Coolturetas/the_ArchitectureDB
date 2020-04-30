3a1bc93bab01534cdeb023176e44bdbedeeee6a4
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-30 22:03:17 +0200

    move graphs to main page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,5 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => res.render('index', { user: req.user }))
-router.get('/charts', (req, res) => res.render('data-visualization'))
 
 module.exports = router
