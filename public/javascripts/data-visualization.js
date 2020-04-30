d099aead4163f36ae77147f8b7421d46287ee334
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-30 14:21:32 +0200

    add new charts

--- public/javascripts/data-visualization.js
+++ public/javascripts/data-visualization.js
@@ -1,6 +1,14 @@
-function printDonutChart(data, colorScale, colorRangeInfo, field, canvas) {
+function printDonutChart(
+  data,
+  colorScale,
+  colorRangeInfo,
+  field,
+  canvas,
+  isPopulated
+) {
   const fields = data.map((object) => object[field])
   let unduplicatedEntries = []
+  let unduplicatedEntriesPop = []
   let workCount = {}
 
   fields.forEach((elm) => {
@@ -11,6 +19,36 @@ function printDonutChart(data, colorScale, colorRangeInfo, field, canvas) {
       workCount[elm]++
     }
   })
+
+  if (isPopulated) {
+    unduplicatedEntries.forEach((elm) => {
+      switch (field) {
+        case 'architect':
+          axios
+            .get(`/api/architects/${elm}`)
+            .then((result) => {
+              unduplicatedEntriesPop.push(result.data.arch.name)
+            })
+            .catch((err) => {
+              next(new Error(err))
+            })
+          break
+        case 'trend':
+          axios
+            .get(`/api/trend/${elm}`)
+            .then((result) => {
+              unduplicatedEntriesPop.push(result.data.trend.name)
+            })
+            .catch((err) => {
+              next(new Error(err))
+            })
+          break
+      }
+    })
+    console.log('Before: ', unduplicatedEntries)
+    unduplicatedEntries = unduplicatedEntriesPop
+    console.log('After: ', unduplicatedEntries)
+  }
 
   const datasetEntries = Object.keys(workCount)
   const worksPerEntry = datasetEntries.map((field) => workCount[field])
@@ -27,8 +65,9 @@ function printDonutChart(data, colorScale, colorRangeInfo, field, canvas) {
     type: 'doughnut',
     data: {
       datasets: [{ backgroundColor: colors, data: worksPerEntry }],
-      labels: datasetEntries,
+      labels: unduplicatedEntries,
     },
+    options: { legend: { position: 'top' } },
   })
 }
 
@@ -36,7 +75,6 @@ axios
   .get('/api/works')
   .then((works) => {
     data = works.data.allWorks
-    console.log(data)
     printDonutChart(
       data,
       d3.interpolatePuRd,
@@ -49,17 +87,50 @@ axios
     console.log(err)
   })
 
-  axios
+axios
   .get('/api/works')
   .then((works) => {
     data = works.data.allWorks
-    console.log(data)
     printDonutChart(
       data,
       d3.interpolatePuRd,
       [0, 1],
       'workType',
       'works-per-type'
+    )
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+axios
+  .get('/api/works')
+  .then((works) => {
+    data = works.data.allWorks
+    printDonutChart(
+      data,
+      d3.interpolatePuRd,
+      [0, 1],
+      'architect',
+      'works-per-architect',
+      true
+    )
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+  axios
+  .get('/api/works')
+  .then((works) => {
+    data = works.data.allWorks
+    printDonutChart(
+      data,
+      d3.interpolatePuRd,
+      [0, 1],
+      'trend',
+      'works-per-trend',
+      true
     )
   })
   .catch((err) => {
