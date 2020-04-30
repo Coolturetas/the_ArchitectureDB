929c52e5a56e8120eef788ea6389711da1437ac1
Yai <myd.rio@hotmail.com>
2020-04-30 17:53:11 +0200

    trend editing working again

--- routes/trend.routes.js
+++ routes/trend.routes.js
@@ -5,19 +5,64 @@ const Work = require('../models/work.model')
 const Comment = require('../models/comment.model')
 const cloudUploader = require('../configs/cloudinary.config')
 
-const checkAuth = (req, res, next) => (req.isAuthenticated() ? next() : res.redirect('/login'))
-const checkIsInRole = (role) => (req, res, next) => (req.isAuthenticated() && req.user.role === role ? next() : res.redirect('/login'))
+function checkAuth(req, res, next) {
+	return req.isAuthenticated() ? next() : res.redirect('/login')
+}
+
+function checkRoles(role) {
+	return function (req, res, next) {
+		return req.isAuthenticated() && req.user.role === role ? next() : res.redirect('/login')
+	}
+}
 
 //Print Architects DB
 router.get('/', (req, res, next) => {
 	Trend.find({ isVerified: true }).then((atFound) => res.render('archTrend/at-index', { atFound, user: req.user }))
 })
 
+//Add new trend
+router.get('/new', checkAuth, (req, res, next) => res.render('archTrend/at-add'))
+
+router.post('/', cloudUploader.single('photo-trend'), checkAuth, (req, res, next) => {
+	let verification = true
+	req.user.role == 'colaborator' ? (verification = false) : null
+
+	let pic
+
+	if (req.file === undefined) {
+		pic = 'https://res.cloudinary.com/dxf11hxhh/image/upload/v1587913924/theArchitectureDB/default_dh4el6.jpg'
+	} else {
+		pic = req.file.url
+	}
+
+	const newTrend = {
+		name: req.body.name,
+		description: req.body.description,
+		country: req.body.country,
+		bestWork: req.body.bestWork,
+		year: req.body.year,
+		isVerified: verification,
+	}
+
+	Trend.create(newTrend, { picTrend: pic })
+		.then(res.redirect('/trend'))
+		.catch((err) => console.log('No se ha creado nada', err))
+})
+
+//Delete
+router.post('/delete/:id', checkAuth, (req, res, next) => {
+	if (req.user.role == 'admin' || req.user.role == 'editor') {
+		Trend.findByIdAndRemove(req.params.id)
+			.then(res.redirect('/trend'))
+			.catch((err) => next(new Error('No se ha borrado nada', err)))
+	}
+})
+
 //Edit
 router.get('/edit/:id', checkAuth, (req, res, next) => {
 	Trend.findById(req.params.id)
-		.then((toEdit) => res.render('archTrend/at-edit', { toEdit, user: req.user }))
-		.catch((err) => next(new Error(err)))
+		.then((toEdit) => res.render('archTrend/at-edit', toEdit))
+		.catch((err) => next(new Error('No se ha encontrado nada para editar', err)))
 })
 
 router.post('/edit/:id', checkAuth, cloudUploader.single('photo-trend'), (req, res, next) => {
@@ -33,45 +78,10 @@ router.post('/edit/:id', checkAuth, cloudUploader.single('photo-trend'), (req, r
 		bestWork: req.body.bestWork,
 		year: req.body.year,
 	}
-	console.log(editTrend)
 
-	// Trend.findByIdAndUpdate(req.params.id, editTrend, { picTrend: pic, new: true })
-	// 	.then((data) => console.log(data))
-	// 	// .then(res.redirect('/trend'))
-	// 	.catch((err) => next(new Error(err)))
-})
-
-//Add new trend
-router.get('/new', checkAuth, (req, res, next) => res.render('archTrend/at-add', { user: req.user }))
-
-router.post('/', cloudUploader.single('photo-trend'), checkAuth, (req, res, next) => {
-	let verification = true
-	req.user.role == 'colaborator' ? (verification = false) : null
-
-	let pic
-	req.file ? (pic = req.file.url) : (pic = 'https://res.cloudinary.com/dxf11hxhh/image/upload/v1587913924/theArchitectureDB/default_dh4el6.jpg')
-
-	const newTrend = {
-		name: req.body.name,
-		description: req.body.description,
-		country: req.body.country,
-		bestWork: req.body.bestWork,
-		year: req.body.year,
-		isVerified: verification,
-	}
-
-	Trend.create(newTrend, { picTrend: pic })
+	Trend.findByIdAndUpdate(req.params.id, editTrend, { picTrend: pic, new: true })
 		.then(res.redirect('/trend'))
-		.catch((err) => next(new Error(err)))
-})
-
-//Delete
-router.post('/delete/:id', checkAuth, (req, res, next) => {
-	if (req.user.role == 'admin' || req.user.role == 'editor') {
-		Trend.findByIdAndRemove(req.params.id)
-			.then(res.redirect('/trend'))
-			.catch((err) => next(new Error(err)))
-	}
+		.catch((err) => next(new Error('No se ha editado nada', err)))
 })
 
 ///
@@ -87,7 +97,7 @@ router.post('/post-comment/:id', checkAuth, (req, res, next) => {
 	}
 	Comment.create(newComment)
 		.then(res.redirect(`/trend/show/${newComment.postedIn}`))
-		.catch((err) => next(new Error(err)))
+		.catch((err) => next(new Error('No se ha posteado comentario', err)))
 })
 //Delete comment
 router.post('/post-comment/delete/:id', checkAuth, (req, res, next) => {
@@ -102,7 +112,7 @@ router.post('/post-comment/delete/:id', checkAuth, (req, res, next) => {
 		})
 		.then((resultId) => Comment.findByIdAndRemove(resultId))
 		.then(() => res.redirect(`/trend/show/${placePosted}`))
-		.catch((err) => next(new Error(err)))
+		.catch((err) => next(new Error('No se ha borrado tu comentario', err)))
 })
 
 //Show details of each trend
@@ -112,7 +122,7 @@ router.get('/show/:id', (req, res, next) => {
 	const promisePost = Comment.find({ postedIn: req.params.id }).populate('creatorId')
 	Promise.all([promiseTrend, promiseWork, promisePost])
 		.then((data) => res.render('archTrend/at-dets', { trend: data[0], works: data[1], posts: data[2], user: req.user }))
-		.catch((err) => next(new Error(err)))
+		.catch((err) => next(new Error('No se ha encontrado nada', err)))
 })
 
 module.exports = router
