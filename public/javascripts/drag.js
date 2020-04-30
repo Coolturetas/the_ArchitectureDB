08148e16e41a32fdef02be25ddbb003da3b029c5
Yai <myd.rio@hotmail.com>
2020-04-30 12:36:48 +0200

    favs check working finally

--- public/javascripts/drag.js
+++ public/javascripts/drag.js
@@ -1,1 +1,14 @@
-dragula([document.querySelector('.is-left'), document.querySelector('.is-right')])
+import Sortable from 'sortablejs'
+
+const wish = document.getElementById('to-wish')
+const visit = document.getElementById('to-visit')
+
+new Sortable(wish, {
+	group: 'shared', // set both lists to same group
+	animation: 150,
+})
+
+new Sortable(visit, {
+	group: 'shared',
+	animation: 150,
+})
