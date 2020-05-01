c564ce14f28c2c03f3bf95d0912b9b7d39d0d811
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-01 11:52:52 +0200

    refactor user signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,42 +11,68 @@ const bcryptSalt = 10
 // User signup
 router.get('/signup', (req, res) => res.render('auth/signup'))
 router.post('/signup', (req, res, next) => {
-	const { username, password } = req.body
+  const { username, password } = req.body
 
-	if (!username || !password) {
-		res.render('auth/signup', { errorMsg: 'Rellena el usuario y la contraseña' })
-		return
-	}
+  if (!username || !password) {
+    res.render('auth/signup', {
+      errorMsg: 'Rellena el usuario y la contraseña',
+    })
+    return
+  }
 
-	User.findOne({ username })
-		.then((user) => {
-			if (user) {
-				res.render('auth/signup', { errorMsg: 'El usuario ya existe en la BBDD' })
-				return
-			}
-			const salt = bcrypt.genSaltSync(bcryptSalt)
-			const hashPass = bcrypt.hashSync(password, salt)
+  User.findOne({ username })
+    .then((user) => {
+      if (user) {
+        res.render('auth/signup', {
+          errorMsg: 'El usuario ya existe en la BBDD',
+        })
+        return
+      }
+      const salt = bcrypt.genSaltSync(bcryptSalt)
+      const hashPass = bcrypt.hashSync(password, salt)
 
-			const createVisitedList = { nameList: 'Lugares que visité', typeOfList: 'visited' }
-			const createWishList = { nameList: 'Me muero por ir', typeOfList: 'wish' }
+      const createVisitedList = {
+        nameList: 'Lugares que visité',
+        typeOfList: 'visited',
+      }
+      const createWishList = { nameList: 'Me muero por ir', typeOfList: 'wish' }
 
-			List.create([createVisitedList, createWishList]).then((listsCreated) => {
-				User.create({ username, password: hashPass, visitedList: listsCreated[0], wishList: listsCreated[1] })
-					.then(() => res.redirect('/'))
-					.catch(() => res.render('auth/signup', { errorMsg: 'No se pudo crear el usuario' }))
-			})
-		})
-		.catch((error) => next(error))
+      List.create([createVisitedList, createWishList])
+        .then((listsCreated) =>
+          User.create({
+            username,
+            password: hashPass,
+            visitedList: listsCreated[0],
+            wishList: listsCreated[1],
+          })
+        )
+        .then(() => res.redirect('/'))
+        .catch(() =>
+          res.render('auth/signup', { errorMsg: 'No se pudo crear el usuario' })
+        )
+    })
+    .catch((error) => next(error))
 })
 
 // User login
-router.get('/login', (req, res) => res.render('auth/login', { errorMsg: req.flash('error') }))
-router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: true, passReqToCallback: true, badRequestMessage: 'Rellena todos los campos' }))
+router.get('/login', (req, res) =>
+  res.render('auth/login', { errorMsg: req.flash('error') })
+)
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+    passReqToCallback: true,
+    badRequestMessage: 'Rellena todos los campos',
+  })
+)
 
 // User logout
 router.get('/logout', (req, res) => {
-	req.logout()
-	res.redirect('/')
+  req.logout()
+  res.redirect('/')
 })
 
 module.exports = router
